03851ea46bdb87aebaefb169cb2468a6cb82e0a4
User <user@Users-MacBook-Pro.local>
2021-07-22 22:56:00 +0300

    exercise
    Committer: Tamir Raz <tamir.raz@gmail.com>

--- vagrant/fixScripts/exercise3-fix.sh
+++ vagrant/fixScripts/exercise3-fix.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 #add fix to exercise3 here
+#I first got:"403 Forbidden You don't have permission to access..."#
+#The issue was with the configuration of apache2: 000-default.conf file contained 'Require all denied' which deny every request of access to this address#
+#To resolve this I edited this configuration file by using this command:#
+
+sudo sed -i -e 's/denied/granted/g' /etc/apache2/sites-available/000-default.conf
--- vagrant/fixScripts/exercise4-fix_server2.sh
+++ vagrant/fixScripts/exercise4-fix_server2.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 #add fix to exercise4-server2 here
+#In order to do so an entry should be added to the host file:#
+sudo sed -i '$ a 192.168.100.10 server1' /etc/hosts
+sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+
+sudo systemctl restart sshd
